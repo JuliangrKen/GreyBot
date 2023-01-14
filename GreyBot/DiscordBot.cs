e6075c2c5bf73c5cfb72ebfcf4d0282d7ede265f
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-14 21:54:25 +0300

    Добавил класс-фасад бота

---
+++ GreyBot/DiscordBot.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GreyBot
+{
+    public class DiscordBot
+    {
+        private readonly BotConfig config;
+        private readonly DiscordSocketClient socketClient;
+        private readonly InteractionService interactionService;
+        private readonly IServiceProvider serviceProvider;
+
+        public DiscordBot
+        (
+            BotConfig config,
+            DiscordSocketClient socketClient,
+            InteractionService interactionService,
+            IServiceProvider serviceProvider
+        )
+        {
+            this.config = config;
+            this.socketClient = socketClient;
+            this.interactionService = interactionService;
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task Run()
+        {
+            socketClient.Log += Log;
+            socketClient.Ready += HandleSlashCommandAsync;
+
+            await socketClient.LoginAsync(TokenType.Bot, config.BotToken);
+            await socketClient.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleSlashCommandAsync()
+        {
+            await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), serviceProvider);
+
+#if DEBUG
+            await interactionService.RegisterCommandsToGuildAsync(config.DevGuildId
+                ?? throw new NullReferenceException());
+#else
+            await interactionService.RegisterCommandsGloballyAsync();
+#endif
+
+            socketClient.InteractionCreated += async interaction =>
+            {
+                var scope = serviceProvider.CreateScope();
+                var ctx = new SocketInteractionContext(socketClient, interaction);
+                await interactionService.ExecuteCommandAsync(ctx, scope.ServiceProvider);
+            };
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+    }
+}
--- GreyBot/Program.cs
+++ GreyBot/Program.cs
@@ -1,10 +1,26 @@
-﻿using GreyBot.Data;
+﻿using GreyBot;
+using GreyBot.Data;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
 services.AddDbContextFactory<GreyBotContext>();
 
+#if DEBUG
+var configPath = $"{Environment.CurrentDirectory}\\BotConfig.Development.json";
+#else
+var configPath = $"{Environment.CurrentDirectory}\\BotConfig.json";
+#endif
+
+services.AddSingleton(BotConfig.GetFromFile(configPath));
+
 var build = services.BuildServiceProvider();
 
 build.GetService<GreyBotContext>();
+
+var bot = build.GetService<DiscordBot>()
+    ?? throw new NullReferenceException();
+
+await bot.Run();
+
+Console.WriteLine("Bye-bye");
