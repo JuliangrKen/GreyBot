6cc55ca291852dd66dbfd6f7507540f9e178315a
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-14 21:14:48 +0300

    Добавил конфиг бота

---
+++ GreyBot/BotConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GreyBot
+{
+    public class BotConfig
+    {
+        public string? BotToken { get; set; }
+        public ulong? DevGuildId { get; set; }
+
+        public static BotConfig GetFromFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+
+            return JsonSerializer.Deserialize<BotConfig>(json)
+                ?? throw new NullReferenceException();
+        }
+    }
+}
