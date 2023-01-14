a6be0bfb1f97c279fb047fb3cf724bf5929cee95
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-14 20:42:20 +0300

    Добавил контекст базы данных

---
+++ GreyBot.Data/GreyBotContext.cs
@@ -0,0 +1,21 @@
+﻿using GreyBot.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreyBot.Data
+{
+    public class GreyBotContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Compliment> Compliments { get; set; }
+        public DbSet<Insult> Insults { get; set; }
+
+        public DbSet<ComplimentLog> ComplimentLogs { get; set; }
+        public DbSet<InsultLog> InsultLogs { get; set; }
+
+        public GreyBotContext(DbContextOptions<GreyBotContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
