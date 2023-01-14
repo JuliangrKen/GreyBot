6ee17009bd25d7a7eb9528914fee3fcbd622a024
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-14 20:42:04 +0300

    Добавил ключевые модели

---
+++ GreyBot.Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreyBot.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public ulong DiscordId { get; set; }
+        public bool HasWhiteList { get; set; }
+    }
+}
