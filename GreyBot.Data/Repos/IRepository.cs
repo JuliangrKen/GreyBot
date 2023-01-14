48f849190120b94937e43a3c22c76990523f432c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-14 20:42:39 +0300

    Добавил обобщенный репозиторий

---
+++ GreyBot.Data/Repos/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace GreyBot.Data.Repos
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(T item);
+    }
+}
---
+++ GreyBot.Data/Repos/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GreyBot.Data.Repos
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public DbSet<T> Set { get; private set; }
+
+        protected DbContext db;
+
+        public Repository(GreyBotContext db)
+        {
+            this.db = db;
+
+            var set = db.Set<T>();
+            set.Load();
+
+            Set = set;
+        }
+
+        public IEnumerable<T> GetAll()
+            => Set;
+
+        public Task Create(T item)
+        {
+            Set.Add(item);
+            db.SaveChanges();
+
+            return Task.CompletedTask;
+        }
+
+        public Task Delete(T item)
+        {
+            Set.Remove(item);
+            db.SaveChanges();
+
+            return Task.CompletedTask;
+        }
+
+        public Task Update(T item)
+        {
+            Set.Update(item);
+            db.SaveChanges();
+
+            return Task.CompletedTask;
+        }
+    }
+}
