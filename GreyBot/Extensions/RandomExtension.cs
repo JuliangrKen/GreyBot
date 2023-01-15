95404e4a7d50a4f1e82606bba0d88bd08714aa82
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-15 19:34:53 +0300

    Добавил класс для расширений Random и метод расширения для генерации рандомных цветов

---
+++ GreyBot/Extensions/RandomExtension.cs
@@ -0,0 +1,10 @@
+﻿using Discord;
+
+namespace GreyBot.Extensions
+{
+    internal static class RandomExtension
+    {
+        public static Color NextColor(this Random random)
+            => new Color(random.Next(byte.MaxValue), random.Next(byte.MaxValue), random.Next(byte.MaxValue));
+    }
+}
