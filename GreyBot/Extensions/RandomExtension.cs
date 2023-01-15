1f3e1b74c0d6522f685fd3af3b9dc4be1c622a43
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-15 19:35:49 +0300

    Упростил по рекомендации компилятора

--- GreyBot/Extensions/RandomExtension.cs
+++ GreyBot/Extensions/RandomExtension.cs
@@ -5,6 +5,6 @@ namespace GreyBot.Extensions
     internal static class RandomExtension
     {
         public static Color NextColor(this Random random)
-            => new Color(random.Next(byte.MaxValue), random.Next(byte.MaxValue), random.Next(byte.MaxValue));
+            => new(random.Next(byte.MaxValue), random.Next(byte.MaxValue), random.Next(byte.MaxValue));
     }
 }
