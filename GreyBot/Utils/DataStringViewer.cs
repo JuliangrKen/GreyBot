42b5d0f5d89da77486fb973f3a1a00e676d8d288
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-21 20:04:35 +0300

    Добавил обобщенный класс для оформления строковых таблицы

---
+++ GreyBot/Utils/DataStringViewer.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace GreyBot.Utils
+{
+    internal class DataStringViewer<T> where T : class
+    {
+        public DataStringViewer(IEnumerable<T> data)
+            => Data = data.ToArray();
+
+        public DataStringViewer(T[] data)
+            => Data = data;
+
+        public T[] Data { get; set; }
+
+        public string GetView(int startIndex, int endIndex, Func<T, string> predicate, Action? completion = null) 
+            => GetView(new StringBuilder(), startIndex, endIndex, predicate, completion);
+
+        public string GetView(StringBuilder stringBuilder, int startIndex, int endIndex, Func<T, string> predicate, Action? completion = null)
+        {
+            if (Data.Length < startIndex)
+                return stringBuilder.ToString();
+
+            endIndex = endIndex > Data.Length ? Data.Length : endIndex;
+
+            foreach (var model in Data[startIndex..endIndex])
+                stringBuilder.Append(predicate.Invoke(model));
+
+            completion?.Invoke();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
