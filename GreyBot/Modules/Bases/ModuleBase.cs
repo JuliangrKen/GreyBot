823fa02a2cdc8e0d7b0c5366308281d7fe5e5185
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-21 21:04:04 +0300

    Добавил базовый модуль

---
+++ GreyBot/Modules/Bases/ModuleBase.cs
@@ -0,0 +1,10 @@
+﻿using Discord.Interactions;
+
+namespace GreyBot.Modules.Bases
+{
+    public class ModuleBase : InteractionModuleBase<SocketInteractionContext>
+    {
+        protected Task WriteErrorMessage()
+            => RespondAsync("Что-то пошло не так!", ephemeral: true);
+    }
+}
