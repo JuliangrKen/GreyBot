a1b4188ebd1f35ca9833d59ff695b2e23c433786
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-22 00:13:18 +0300

    Добавил модуль для говорения от лица бота и первую команду, использующую модальное окно

---
+++ GreyBot/Modules/TalkModule.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using GreyBot.Extensions;
+using GreyBot.Modules.Bases;
+
+namespace GreyBot.Modules
+{
+    [Group("talk", "Говорение он лица бота")]
+    public class TalkModule : ModuleBase
+    {
+        private const string TalkEmbedId = "talk_embed";
+        private const string TalkEmbedTitleId = "talk_embed_title";
+        private const string TalkEmbedTextId = "talk_embed_text";
+
+        private static TalkModule? Singleton;
+
+        public TalkModule(DiscordSocketClient socketClient)
+        {
+            if (Singleton != null) return;
+            Singleton = this;
+
+            socketClient.ModalSubmitted += BotWriteEmbedHandle;
+        }
+
+        [SlashCommand("embed", "Написать embed")]
+        public async Task BotWriteEmbed()
+        {
+            var modalBuilder = new ModalBuilder()
+                .WithTitle("Embed")
+                .WithCustomId(TalkEmbedId)
+                .AddTextInput("Заголовок", TalkEmbedTitleId, TextInputStyle.Short, "Чё-то важное", null, 30)
+                .AddTextInput("Основной текст", TalkEmbedTextId, TextInputStyle.Paragraph, "Я вам расскажу, откуда готовилось нападение на Беларусь");
+
+            await Context.Interaction.RespondWithModalAsync(modalBuilder.Build());
+        }
+
+        private async Task BotWriteEmbedHandle(SocketModal modal)
+        {
+            var components = modal.Data.Components;
+
+            var embedBuilder = new EmbedBuilder()
+                .WithAuthor(modal.User)
+                .WithColor(new Random().NextColor())
+                .WithTitle(components.FirstOrDefault((c) => c.CustomId == TalkEmbedTitleId)?.Value)
+                .WithDescription(components.FirstOrDefault((c) => c.CustomId == TalkEmbedTextId)?.Value);
+
+            await modal.RespondAsync(embed: embedBuilder.Build());
+        }
+    }
+}
