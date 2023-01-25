0359d26295da062461f90d531bba832213371805
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-01-25 22:37:38 +0300

    Сделал класс абстрактным

--- GreyBot/Modules/Bases/ModuleBase.cs
+++ GreyBot/Modules/Bases/ModuleBase.cs
@@ -2,7 +2,7 @@
 
 namespace GreyBot.Modules.Bases
 {
-    public class ModuleBase : InteractionModuleBase<SocketInteractionContext>
+    public abstract class ModuleBase : InteractionModuleBase<SocketInteractionContext>
     {
         protected Task WriteErrorMessage()
             => RespondAsync("Что-то пошло не так!", ephemeral: true);
